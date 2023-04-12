c029d7226b101d99087926611f3938caf4c17904
sinan234 <sinanmp234@gmail.com>
2023-04-12 15:28:39 +0530

    razorpay integrated

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ connectDb().then(async () => {
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/products')
+var paymentRouter = require('./routes/payment')
 var userAuth = require('./middlewares/auth/userAuth')
 
 var app = express();
@@ -74,6 +75,7 @@ app.use('/virtual-cart', () => {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productRouter);
+app.use('/payment', paymentRouter)
 
 app.get('/login', userAuth, function (req, res) {
   res.send("login")
