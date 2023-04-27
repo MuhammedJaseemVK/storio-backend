72c9eb929c51e7c14bde62296add01604c6c4d40
sinan234 <sinanmp234@gmail.com>
2023-04-27 16:00:00 +0530

    payment bill fetching temp

--- routes/payment.js
+++ routes/payment.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const Razorpay = require("razorpay");
+const Product = require('../models/Product');
 
 const router = express.Router();
 
@@ -12,7 +13,7 @@ router.post("/orders", async (req, res) => {
         });
 
         const options = {
-            amount: 50000, // amount in smallest currency unit
+            amount: req.body.total, // amount in smallest currency unit
             currency: "INR",
             receipt: "receipt_order_74394",
         };
@@ -22,6 +23,22 @@ router.post("/orders", async (req, res) => {
         if (!order) return res.status(500).send("Some error occured");
 
         res.json(order);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+router.post("/fetchBill", async (req, res) => {
+    try {
+        let products = req.body.products
+        const product = await Product.findById(products[0]);
+        if (product == null) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        
+        res.json({
+            products: product,
+            total: product.price
+        });
     } catch (error) {
         res.status(500).send(error);
     }
